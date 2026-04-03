c01924abd3dbdaec6724a8f741addd7c216b1228
Polymer-505 <korololeksandr306@gmail.com>
2026-04-03 14:05:05 +0300

    Трохи поробив і додав стилі для title та для полоски

---
+++ src/js/year-of-birth.js
@@ -0,0 +1,23 @@
+const inputYear = document.querySelector('.year-of-birth__input');
+const btnYear = document.querySelector('.year-of-birth__btn');
+const textYear = document.querySelector('.year-of-birth__text');
+
+btnYear.addEventListener('click', () => {
+  const year = parseInt(inputYear.value);
+
+  if (isNaN(year) || inputYear.value.trim() === '') {
+    textYear.textContent = 'Будь ласка, введіть рік!';
+    textYear.style.color = 'red';
+    return;
+  }
+
+  const isLeapYear = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+
+  if (isLeapYear) {
+    textYear.textContent = 'Ви народилися у високосний рік!';
+    textYear.style.color = 'green';
+  } else {
+    textYear.textContent = 'Ви народилися не у високосний рік!';
+    textYear.style.color = 'red';
+  }
+});
