e5dead6e79e8d74df4c920a3904c2baac9713c02
Kamilla-goddes <kamillakamisya@gmail.com>
2026-04-04 14:32:50 +0300

    clean cache

---
+++ src/js/calculator.js
@@ -0,0 +1,20 @@
+const elements = {
+    first: document.querySelector('.calculator-1'),
+    second: document.querySelector('.calculator-2'),
+    result: document.querySelector('.result'),
+    plus: document.querySelector('.plus'),
+    minus: document.querySelector('.minus'),
+    multiply: document.querySelector('.multiply'),
+    divide: document.querySelector('.divide'),
+    equals: document.querySelector('.equals'),
+  }
+  
+  elements.plus.onclick = () => operation = (a, b) => a + b;
+  elements.minus.onclick = () => operation = (a, b) => a - b;
+  elements.multiply.onclick = () => operation = (a, b) => a * b;
+  elements.divide.onclick = () => operation = (a, b) => a / b;
+  elements.equals.onclick = () => {
+    const a = + elements.first.value;
+    const b = + elements.second.value;
+    elements.result.value = operation(a, b);
+  }
