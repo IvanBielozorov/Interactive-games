54ebee6c9bb784248fb9bf9af8681848bda7cb44
Alina <krasnanikalina129@gmail.com>
2026-04-05 20:46:32 +0300

    добавила знову код

---
+++ src/js/three-numbers.js
@@ -0,0 +1,30 @@
+const refs = {
+  input: document.querySelectorAll('.three-numbers_input'),
+  result: document.querySelector('.three-numbers_result'),
+};
+
+function updateNumberArray() {
+  let numbers = [];
+
+  refs.input.forEach(input => {
+    const value = input.value;
+
+    if (value === '') return; 
+
+    const num = Number(value);
+    numbers.push(num);
+  });
+
+  if (numbers.length === 0) {
+    refs.result.textContent = 'Введіть число';
+    return;
+  }
+  console.log(numbers);
+  
+  const maxNumber = Math.max(...numbers);
+  refs.result.textContent = `Найбільше число, яке ви ввели - ${maxNumber}`;
+}
+
+refs.input.forEach(input => {
+  input.addEventListener('input', updateNumberArray);
+});
