fc9e2222c89af12f5fe3ee3ed593afdbb7293a10
IvanBielozorov <ivanbelozorov2020@gmail.com>
2026-04-06 15:46:58 +0100

    Some scientist section start

--- src/js/choose-a-scientist.js
+++ src/js/choose-a-scientist.js
@@ -84,3 +84,136 @@ const scientists = [
     id: 12,
   },
 ];
+// References
+const refs = {
+  containerScientistsEls: document.querySelector('.scientists-container-js'),
+  titleEl: document.createElement('h2'),
+  scientistsGridEl: document.createElement('ul'),
+  scientistsEls: [],
+  buttonsGridEl: document.createElement('ul'),
+  buttonEls: [],
+};
+
+// Title
+refs.titleEl.textContent = 'Обери вченого/их';
+refs.titleEl.classList.add('title');
+refs.containerScientistsEls.appendChild(refs.titleEl);
+// Creating scinetists
+
+const images = [
+  './#',
+  './#',
+  './#',
+  './#',
+  './#',
+  './#',
+  './#',
+  './#',
+  './#',
+  './#',
+  './#',
+  './#',
+];
+
+refs.scientistsGridEl.classList.add('scientists-grid');
+
+scientists.forEach((scientist, index) => {
+  const li = document.createElement('li');
+  const img = document.createElement('img');
+  img.src = images[index] || './#'; // fallback if no image
+  img.alt = `${scientist.name} ${scientist.surname}`;
+  li.appendChild(img);
+
+  refs.scientistsGridEl.appendChild(li);
+  refs.scientistsEls.push(li);
+  li.classList.add('scientists-el');
+});
+
+refs.containerScientistsEls.appendChild(refs.scientistsGridEl);
+
+// Creating buttons with its text
+refs.buttonsGridEl.classList.add('scientists-buttons-grid');
+
+const buttonTexts = [
+  'Які вчені народилися в 19 ст.',
+  'Знайти рік народження Albert Einshtein',
+  'Відсортувати вчених за алфавітом',
+  'Знайти вчених, прізвища яких починаються на на літеру “С” ',
+  'Відсортувати вчених за кількістю прожитих років',
+  'Видалити всіх вчених, ім’я яких починається на “А”',
+  'Знайти вченого, який народився найпізніше',
+  'Знайти вченого, який прожив найдовше і вченого, який прожив найменше',
+  'Знайти вчених, в яких співпадають перші літери імені і прізвища',
+];
+
+buttonTexts.forEach(text => {
+  const li = document.createElement('li');
+  const button = document.createElement('button');
+  button.textContent = text; // assign the unique text
+  button.classList.add('scientists-buttons');
+  li.appendChild(button);
+  refs.buttonsGridEl.appendChild(li);
+  refs.buttonEls.push(button); // store the button in refs
+});
+refs.containerScientistsEls.appendChild(refs.buttonsGridEl);
+
+refs.buttonEls[7].style.height = '50px';
+
+// Functions for buttons
+
+// First button
+refs.buttonEls[0].addEventListener('click', onFirstButtonClicked);
+
+function onFirstButtonClicked() {
+  refs.scientistsEls.forEach(li => {
+    refs.scientistsGridEl.appendChild(li);
+  });
+  scientists.forEach((scientist, i) => {
+    if (!(scientist.born >= 1800 && scientist.born <= 1900)) {
+      refs.scientistsEls[i].remove();
+    }
+  });
+}
+
+// Second button
+refs.buttonEls[1].addEventListener('click', onSecondButtonClicked);
+
+function onSecondButtonClicked() {
+  scientists.forEach(scientist => {
+    if (scientist.name === 'Albert' && scientist.surname === 'Einstein') {
+      alert(`Рік народження Енштейна ${scientist.born}`);
+    }
+  });
+}
+
+// Third button
+refs.buttonEls[2].addEventListener('click', onThirdButtonClicked);
+
+function onThirdButtonClicked() {
+  refs.scientistsEls.forEach(li => {
+    refs.scientistsGridEl.appendChild(li);
+  });
+  const sortedScientists = [...scientists].sort((a, b) =>
+    a.surname.localeCompare(b.surname)
+  );
+  sortedScientists.forEach(sortedScientist => {
+    const index = scientists.findIndex(s => s.id === sortedScientist.id);
+    const li = refs.scientistsEls[index];
+    refs.scientistsGridEl.appendChild(li);
+  });
+}
+
+// Forth button
+refs.buttonEls[3].addEventListener('click', onForthButtonClicked);
+
+function onForthButtonClicked() {
+  refs.scientistsEls.forEach(li => {
+    refs.scientistsGridEl.appendChild(li);
+  });
+  scientists.forEach((scientist, i) => {
+    const surnameLetters = scientist.surname.split('');
+    if (surnameLetters[0] != 'C') {
+      refs.scientistsEls[i].remove();
+    }
+  });
+}
