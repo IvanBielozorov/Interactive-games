d900cba74d54d71d92d009aecc280457bde8ffb7
IvanBielozorov <ivanbelozorov2020@gmail.com>
2026-04-20 18:45:25 +0100

    Repaired website

---
+++ src/js/modal-gratitude.js
@@ -0,0 +1,29 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-footer-modal-open]'),
+    closeModalBtn: document.querySelector('[data-footer-modal-close]'),
+    modal: document.querySelector('[data-footer-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  refs.modal.addEventListener('click', onBackdropClick);
+  document.addEventListener('keydown', onEscPress);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle('no-scroll');
+  }
+  function onBackdropClick(e) {
+    if (e.target === refs.modal) {
+      toggleModal();
+    }
+  }
+
+  function onEscPress(e) {
+    if (e.key === 'Escape' && !refs.modal.classList.contains('is-hidden')) {
+      toggleModal();
+    }
+  }
+})();
