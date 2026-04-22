0144b15016f86dad86adb2c0bfbefd165f2c3ad0
Kamilla-goddes <kamillakamisya@gmail.com>
2026-04-22 23:00:09 +0300

    додала сортування секцій

--- src/js/header.js
+++ src/js/header.js
@@ -3,3 +3,33 @@ const btn = document.getElementById('themeToggle');
 btn.addEventListener('click', () => {
   document.body.classList.toggle('dark-theme');
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('[data-filter]');
+    const sections = document.querySelectorAll('section[id]');
+  
+    const groups = {
+      number: ['1', '2', '4', '5', '8'],
+      game: ['3', '6', '7'],
+      intro: ['9', '10']
+    };
+  
+    links.forEach(link => {
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+  
+        const filter = link.dataset.filter;
+        const allowed = groups[filter];
+  
+        sections.forEach(section => {
+          const id = section.id;
+  
+          if (allowed.includes(id)) {
+            section.style.display = 'block';
+          } else {
+            section.style.display = 'none';
+          }
+        });
+      });
+    });
+  });
